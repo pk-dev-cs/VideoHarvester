f4765e919fb18d76c2c2cba7c050f500adcd92d3
pk-dev-cs <kleszcz@mail.com>
2025-03-12 00:55:00 +0100

    move converters to separate directory

--- VideoHarvester/Converters/StatusColorConverter.cs
+++ VideoHarvester/Converters/StatusColorConverter.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
 
-namespace VideoHarvester;
+namespace VideoHarvester.Converters;
 
 public class StatusColorConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         if (value is string status && status == "Downloaded")
-        {
             return Brushes.Green;
-        }
+
         return Brushes.Yellow; // Default color
     }
 
