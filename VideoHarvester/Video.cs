6763b786e87519cb77ae9b8ba82049f1d7319ccc
Przemysław Kleszcz (PHE) <PHE@kmd.dk>
2025-03-11 16:56:41 +0100

    update layout

---
+++ VideoHarvester/Video.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.ComponentModel;
+using System.IO;
+
+namespace VideoHarvester;
+
+public class Video : INotifyPropertyChanged
+{
+    private int _progress;
+    private string _status = "Queued";  // Default status
+
+    public required string VideoId { get; set; }
+    public int Order { get; set; }
+
+    public int Progress
+    {
+        get => _progress;
+        set
+        {
+            _progress = value;
+            OnPropertyChanged(nameof(Progress));
+        }
+    }
+
+    public string Status
+    {
+        get => _status;
+        set
+        {
+            _status = value;
+            OnPropertyChanged(nameof(Status));
+        }
+    }
+
+    public string FilePath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos), $"{Order}.mp4");
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
---
+++ VideoHarvester/WidthAdjustmentConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace VideoHarvester;
+
+public class WidthAdjustmentConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double originalWidth)
+        {
+            return originalWidth - 30; // Subtract 10 pixels
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException(); // One-way binding only
+    }
+}
